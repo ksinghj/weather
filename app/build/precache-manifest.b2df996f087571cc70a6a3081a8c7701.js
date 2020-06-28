594a245c5b7852645e66c33e0783751afeeb1915
Kartar Singh Jabanda <kartarjabanda@hotmail.co.uk>
2020-06-28 16:34:13 +0100

    buikd

---
+++ app/build/precache-manifest.b2df996f087571cc70a6a3081a8c7701.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dd7f1a3f235f8824e67dc87973f0de7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8894d94b636c37071ee",
+    "url": "/static/css/main.75c22f92.chunk.css"
+  },
+  {
+    "revision": "494a06ed78d217d02903",
+    "url": "/static/js/2.13b3338d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.13b3338d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8894d94b636c37071ee",
+    "url": "/static/js/main.18431caa.chunk.js"
+  },
+  {
+    "revision": "817d2bf81990a1d39ee2",
+    "url": "/static/js/runtime-main.6537f79a.js"
+  },
+  {
+    "revision": "b358f7db35068dd33c901eb7850e1d87",
+    "url": "/static/media/sunrise.b358f7db.svg"
+  },
+  {
+    "revision": "a44d55e2ea3995c49cb1149577572cc5",
+    "url": "/static/media/sunset.a44d55e2.svg"
+  }
+]);
